c0920bcbbd2adc9f22bf6d62565e684198bc787a
Vitaliy Zarubin <keygenqt@gmail.com>
2021-10-05 09:36:26 +0300

    first commit

---
+++ GitHubViewer/GitHubViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubViewerApp.swift
+//  GitHubViewer
+//
+//  Created by Виталий Зарубин on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '14.0'
+
+target 'GitHubViewer' do
+  pod 'AFNetworking', '~> 4.0'
+  pod 'SwiftLint'
+end
